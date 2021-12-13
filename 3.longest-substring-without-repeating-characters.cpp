2e83521c87765216620aeb5cdfde34e555a564f1
Ashutosh <ashutoshkumarpandey183@gmail.com>
2021-12-13 17:23:21 +0530

    Updated with O(1) space solution

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -8,17 +8,38 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int size = s.size();
-        int leftIndex = 0, maxSize = 0;
-        unordered_set<int>sets;
+        // int size = s.size();
+        // int leftIndex = 0, maxSize = 0;
+        // unordered_set<int>sets;
 
-        for(int i=0; i<size; ++i){
-            while(sets.find(s[i]) != sets.end()){
-                sets.erase(s[leftIndex]);
-                leftIndex++;
+        // for(int i=0; i<size; ++i){
+        //     while(sets.find(s[i]) != sets.end()){
+        //         sets.erase(s[leftIndex]);
+        //         leftIndex++;
+        //     }
+        //     sets.insert(s[i]);
+        //     maxSize = max(maxSize, i-leftIndex+1);
+        // }
+        // return maxSize;
+        int maxSize = 0;
+        if(s.size() == 0)
+            return 0;
+        if(s.size() == 1)
+            return 1;
+        int n = s.size();
+        vector<int>count(256, 0);
+        int i = 0, j = 0;
+        count[s[0]]++;
+        while(j != n-1){
+            if(count[s[j+1]] == 0){
+                j++;
+                count[s[j]] = 1;
+                maxSize = max(maxSize, j-i+1);
             }
-            sets.insert(s[i]);
-            maxSize = max(maxSize, i-leftIndex+1);
+            else{
+                count[s[i]]--;
+                i++;
+            }
         }
         return maxSize;
     }
