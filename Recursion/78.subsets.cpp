9f64939fb884976e0e8a0034f0130368207ba13d
Ashutosh <ashutoshkumarpandey183@gmail.com>
2021-12-20 15:47:47 +0530

    Updated with bitmasking solution

--- Recursion/78.subsets.cpp
+++ Recursion/78.subsets.cpp
@@ -7,6 +7,26 @@
 // @lc code=start
 class Solution {
 public:
+    // void helper(vector<int>&nums, int idx, int sz, vector<vector<int>>&ans, int tmp[], int size){
+    //     if(idx == size){
+    //         vector<int>arr;
+    //         for(int i=0; i<sz; ++i){
+    //             arr.push_back(tmp[i]);
+    //         }
+    //         ans.push_back(arr);
+    //         return;
+    //     }
+    //     tmp[sz]=nums[idx];
+    //     helper(nums, idx+1, sz+1, ans, tmp, size);
+    //     helper(nums, idx+1, sz, ans, tmp, size);
+    // }
+    // vector<vector<int>> subsets(vector<int>& nums) {
+    //     vector<vector<int>>ans;
+    //     int tmp[nums.size()];
+    //     int size = nums.size();
+    //     helper(nums, 0, 0, ans, tmp, size);
+    //     return ans;
+    // }
     vector<vector<int>> subsets(vector<int>& nums) {
         int num = nums.size();
         int noOfSubsets = pow(2, num);
